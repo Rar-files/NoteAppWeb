f79a0eb745993b7293e23582160ddca9c3b0beb7
rar-files <dwidlak@outlook.com>
2023-10-23 14:45:38 +0200

    RemoveDefaultTemplateContent

---
+++ note-app/src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import type { AppProps } from 'next/app'
+
+const App: FC<AppProps> = ({ Component, pageProps }) => {
+    return (
+        <>
+            <Component {...pageProps} />
+        </>
+    )
+}
+
+export default App
